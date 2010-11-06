efa9ae5d2442677568729d3da56398535e90e52b
Ray Miller <ray@1729.org.uk>
2010-11-06 18:27:03 +0000

    Make a small improvement to guess-alphabet

--- src/Bio/sequence.clj
+++ src/Bio/sequence.clj
@@ -1,15 +1,17 @@
 (ns Bio.sequence)
 
-;; The heuristic below is borrowed from BioPerl's Bio::PrimarySeq::_guess_alphabet
+;; The heuristic below is borrowed from BioPerl's Bio::PrimarySeq->_guess_alphabet.
+;; It suffers the same probem ("N" represents "aNy" in a DNA sequence or Asparagine
+;; in a protein sequence).
 
 (defn guess-alphabet [sequence]
   "Guess the alphabet (:dna, :rna or :protein) of sequence; returns nil for
   empty sequence."
   (when-let [s (seq (filter (comp not #{\. \-}) (seq sequence)))]
     (let [total (count s)
-	  u     (count (filter #{\U \u} s))
-          acgt  (count (filter #{\A \C \G \T \N \a \c \g \t \n} s))]
+	  acgu  (filter #{\A \C \G \U \N \a \c \g \u \n} s)
+          acgt  (filter #{\A \C \G \T \N \a \c \g \t \n} s)]
       (cond
-       (> (/ acgt total) 0.85) :dna
-       (> (/ (+ acgt u) total) 0.85) :rna
+       (> (/ (count acgt) total) 0.85) :dna
+       (> (/ (count acgu) total) 0.85) :rna
        :else :protein))))
